9d6d9ed4a78619348f6d61df69c0b90fb716bbf4
shinshARK <137866950+shinshARK@users.noreply.github.com>
2024-01-05 15:31:08 +0700

    Time: 85 ms (93.42%), Space: 70 MB (39.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -6,14 +6,10 @@ public:
 
         for(const auto& num : nums) {
             freq[num]++;
-            // auto res = freq.emplace(num, 1);
-            // if(!res.second) res.first->second++;
+
             if(freq[num] > 1) return true;
         } 
 
-        // for(const auto& pair : freq) {
-        //     if(pair.second > 1) return true;
-        // }
 
         return false;
         
