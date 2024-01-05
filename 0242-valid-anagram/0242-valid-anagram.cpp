e309ea24cf6afd0b9fc92fdf1542ac2d598bb2ba
shinshARK <137866950+shinshARK@users.noreply.github.com>
2024-01-05 15:49:29 +0700

    Time: 7 ms (73.91%), Space: 7.8 MB (19.85%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.length() != t.length()) return false;
+
+        int s_freq[26] = {0}, t_freq[26] = {0};
+
+        for(const auto& character : s) {
+            s_freq[character - 'a']++;
+        }
+
+        for(const auto& character : t) {
+            t_freq[character - 'a']++;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(s_freq[i] != t_freq[i]) return false;
+        }
+        
+        return true;
+
+        // if inputs contain unicode, use unordered_map ?
+
+        
+
+    }
+};
