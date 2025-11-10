d185936b08bc1ca28671f85a22b50e9e656a2b53
Shinsha <137866950+shinshARK@users.noreply.github.com>
2025-11-10 21:40:36 +0700

    1580-SHUFFLE-THE-ARRAY Stats: Time: 0 ms (100%), Space: 5.3 MB (95%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.go
+++ 1580-shuffle-the-array/1580-shuffle-the-array.go
@@ -1,14 +1,17 @@
 func shuffle(nums []int, n int) []int {
-    // using a new array
-    shuffled := make([]int, n * 2)
-    fi, se := 0, n 
-    for i := 0; i < (n * 2) - 1; i+=2 {
-        shuffled[i] = nums[fi]
-        fi++
-        shuffled[i+1] = nums[se]
-        se++      
-    }
+    numsCopy := make([]int, n * 2)
+    _ = copy(numsCopy, nums)
+    xIndex := 0;
+    yIndex := n;
+    for i := 0; i < n * 2 - 1; i++ {
+        if i % 2 == 0 {
+            nums[i] = numsCopy[xIndex];
+            xIndex++;
+        } else {
+            nums[i] = numsCopy[yIndex];
+            yIndex++;
+        }
 
-    return shuffled
-    // inplace
+    }
+    return nums
 }
