0da2912ed28f693f75f7095180c20a51212bffaa
Shinsha <137866950+shinshARK@users.noreply.github.com>
2025-11-10 20:16:04 +0700

    0027-REMOVE-ELEMENT Stats: Time: 0 ms (100%), Space: 4 MB (67.35%) - LeetHub

--- 0027-remove-element/0027-remove-element.go
+++ 0027-remove-element/0027-remove-element.go
@@ -1,25 +1,10 @@
 func removeElement(nums []int, val int) int {
-    count := 0
-    length := len(nums)
-    rightIndex := length - 1;
-    for i := 0; i < length; i++ {
-        if nums[i] == val {
-            for ; rightIndex > i && rightIndex > 0; {
-                if nums[rightIndex] != val {
-                    break;
-                }
-                rightIndex--;
-            }
-            nums[i] = nums[rightIndex];
-            if rightIndex != 0 {
-                rightIndex--;
-            }
-        } else {
-            count++;
+    write := 0
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != val{
+            nums[write] = nums[i]
+            write++
         }
     }
-    return count
+    return write
 }
-
-
-
